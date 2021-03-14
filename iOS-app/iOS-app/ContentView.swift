6920b4cbabf5531a5ac48031c6bcabd62e3ab693
Thurman1776- <thurman.dev1776@gmail.com>
2021-03-14 18:25:40 +0100

    Split currencies creation

--- iOS-app/iOS-app/ContentView.swift
+++ iOS-app/iOS-app/ContentView.swift
@@ -12,14 +12,28 @@ struct MainView: View {
     @EnvironmentObject
     private var store: ReduxStore
     var body: some View {
-        VStack(spacing: 8) {
-            Text("Frankfurter app")
-            
-            List(store.appState.currencyList.currencies, id: \.id) { currency in
-                Text("Currency: \(currency.fullName)")
-                Text("Currency code: \(currency.code)")
-                    .padding()
+        NavigationView {
+            VStack(spacing: 8) {
+                NavigationLink(destination: CurrencyListView(store.appState.currencyList.currencies)) {
+                    Text("See available currencies")
+                        .foregroundColor(.blue)
+                }
             }
+            .navigationBarTitle("Frankfurter app")
+        }
+    }
+}
+
+private struct CurrencyListView: View {
+    private let currencies: [CurrencyList.Currency]
+    
+    init(_ currencies: [CurrencyList.Currency]) {
+        self.currencies = currencies
+    }
+    
+    var body: some View {
+        List(currencies, id: \.id) { currency in
+            Text("Currency: \(currency.fullName) - \(currency.code)")
         }
     }
 }
