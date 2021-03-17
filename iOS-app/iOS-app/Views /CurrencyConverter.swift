6f380f748ee610de2677444272366ae7da9bc065
Thurman1776- <thurman.dev1776@gmail.com>
2021-03-17 18:02:30 +0100

    Refine bindings

--- iOS-app/iOS-app/Views /CurrencyConverter.swift
+++ iOS-app/iOS-app/Views /CurrencyConverter.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 import Redux
 
 struct CurrencyConverter: View {
@@ -14,13 +15,25 @@ struct CurrencyConverter: View {
     @Environment(\.dispatcher)
     private var dispatcher
     
+    @State
+    private var sourceAmount: String = "0"
+    @State
+    private var destAmount: String = "0"
+    
+    @State
+    private var sourceCurrency: String = CurrencyList.Currency.baseCurrency.code
+    @State
+    private var destCurrency: String = CurrencyList.Currency.baseCurrency.code
+    
     var body: some View {
         if store.appState.currencyList.currencies.isEmpty {
             Text("Download currencies first")
         } else {
             VStack(spacing: 24) {
-                CurrencyInput(title: "Source")
-                CurrencyInput(title: "Destination")
+                CurrencyInput(title: "Source", initialValue: $sourceAmount, defaultCurrency: $sourceCurrency)
+                CurrencyInput(title: "Destination", initialValue: $destAmount, defaultCurrency: $destCurrency)
+                Button("Convert!") {
+                }
             }
         }
     }
@@ -29,14 +42,22 @@ struct CurrencyConverter: View {
 // MARK: Private views
 
 private struct CurrencyInput: View {
-    @State
-    private var amount: String = ""
+    @Binding
+    private var amount: String
+    @Binding
+    private var currency: String
     @EnvironmentObject
     private var store: ReduxStore
     private let title: String
     
-    init(title: String) {
+    init(
+        title: String,
+        initialValue: Binding<String>,
+        defaultCurrency: Binding<String>
+    ) {
         self.title = title
+        self._amount = initialValue
+        self._currency = defaultCurrency
     }
     
     var body: some View {
@@ -45,32 +66,43 @@ private struct CurrencyInput: View {
             TextField(
                 "Enter amount",  text: $amount, onCommit: { dismissKeyboard() }
             )
+            .onReceive(Just(amount), perform: formatString(_:))
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .keyboardType(.numbersAndPunctuation)
-            PickerView(currencies: store.appState.currencyList.currencies)
+            PickerView(currencies: store.appState.currencyList.currencies, currencyRawValue: $currency)
+        }
+    }
+    
+    private func formatString(_ value: String) {
+        let formater = NumberFormatter()
+        formater.numberStyle = .decimal
+        formater.locale = Locale(identifier: "de_DE")
+        
+        if let number = formater.number(from: value) {
+            amount = number.stringValue
         }
     }
 }
 
 private struct PickerView: View {
-    @State
-    private var selectedCurrency: CurrencyList.Currency = .baseCurrency
+    @Binding
+    private var selectedCurrencyCode: String
     @State
     private var shouldShowPicker = false
-    private var currencies: [CurrencyList.Currency] = []
+    private var currencies: [String] = []
     
-    init(currencies: [CurrencyList.Currency]) {
-        self.currencies = currencies
-        self.selectedCurrency = self.currencies.first ?? .baseCurrency
+    init(currencies: [CurrencyList.Currency], currencyRawValue: Binding<String>) {
+        self.currencies = currencies.map { $0.code }
+        self._selectedCurrencyCode = currencyRawValue
     }
 
     var body: some View {
         VStack {
-            Button("\(selectedCurrency.code)", action: { shouldShowPicker.toggle() })
+            Button("\(selectedCurrencyCode)", action: { shouldShowPicker.toggle() })
             if shouldShowPicker {
-                Picker("Currency", selection: $selectedCurrency) {
+                Picker("Currency", selection: $selectedCurrencyCode) {
                     ForEach(currencies, id: \.self) {
-                        Text($0.code)
+                        Text($0)
                     }
                 }
             }
